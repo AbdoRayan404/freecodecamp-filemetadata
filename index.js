5bf06c0ff7695814284cc8b75cdc0d23a5c46a34
Terry-404 <gameocnrer71@gmail.com>
2022-05-09 09:27:50 +0200

    POST api route '/api/fileanalyse'

--- index.js
+++ index.js
@@ -13,8 +13,9 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next)=>{
+  res.json({"name":req.file.originalname,"type":req.file.mimetype,"size":req.file.size});
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
