39e24ca9fb0cd131d5a9d3a4dc118cab9005061a
Terry-404 <gameocnrer71@gmail.com>
2022-05-09 09:22:53 +0200

    setting up the server with multer

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
 require('dotenv').config()
 
-var app = express();
+const app = express();
+const upload = multer({ dest : 'uploads/' })
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
